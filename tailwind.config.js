02bb6f12c60580a90a165d8ad2afa69a2768cfb8
Abhishek F <95033966+avikekk@users.noreply.github.com>
2025-04-08 18:52:05 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: {
+          900: '#000000',
+          800: '#111111',
+          700: '#1a1a1a'
+        }
+      },
+      fontFamily: {
+        sans: ['Inter var', 'system-ui', 'sans-serif'],
+        mono: ['JetBrains Mono', 'monospace'],
+      },
+    },
   },
   plugins: [],
 };
